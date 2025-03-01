b3439544d3c570e10f1107d827c3942292a68e61
ryannguyen <nguyenthaibao.haugiang@gmail.com>
2025-03-01 21:42:40 +0700

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "var(--background1)",
         foreground: "var(--foreground)",
       },
     },
