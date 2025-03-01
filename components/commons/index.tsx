32bae5d533f91be9d4c876c296f309b99cec484a
ryannguyen <nguyenthaibao.haugiang@gmail.com>
2025-03-01 23:10:03 +0700

    Update index - Add accorditions

--- components/commons/index.tsx
+++ components/commons/index.tsx
@@ -47,4 +47,5 @@ export const components = {
   accorditions: dynamic(() => import("./accorditions")),
   accorditions: dynamic(() => import("./accorditions")),
   accorditions: dynamic(() => import("./accorditions")),
+  accorditions: dynamic(() => import("./accorditions")),
 };
